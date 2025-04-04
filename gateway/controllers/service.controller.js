b7da67e6cfb57b196230b7477ae3fecad26464c0
levihoang02 <129204351+levihoang02@users.noreply.github.com>
2025-04-04 19:02:59 +0700

    feat: ai-interface

--- gateway/controllers/service.controller.js
+++ gateway/controllers/service.controller.js
@@ -69,53 +69,6 @@ const createBulkInstances = async (instances) => {
     await ServiceInstance.bulkCreate(instances);
 };
 
-const createNewServiceInstance = asyncErrorHandler(async (req, res, next) => {
-    const name = req.body.name;
-    const host = req.body.host;
-    const port = req.body.port;
-    const endPoint = req.body.endPoint || null;
-
-    try {
-        const [service, created] = await Service.findOrCreate({
-            where: {
-                Sname: name,
-            },
-        });
-
-        const instanceName = generateInstanceName(name);
-        const newInstance = await ServiceInstance.create({
-            id: instanceName,
-            host: host,
-            port: port,
-            endPoint: endPoint,
-            status: true,
-            serviceId: service.id,
-        });
-
-        const payload = {
-            name: instanceName,
-            host: host,
-            port: port,
-        };
-
-        let event = {
-            id: generateId(),
-            source: 'gateway',
-            type: 'CREATED_NEW_SERVICE',
-            payload: payload,
-        };
-
-        sendMessage('dashboard', event);
-        res.status(200).json({ message: 'success' });
-    } catch (err) {
-        console.log(err);
-        const error = new CustomError('Create service error', 500);
-        next(error);
-    } finally {
-        await producer.disconnect();
-    }
-});
-
 const deleteService = asyncErrorHandler(async (req, res, next) => {
     const id = req.body.id;
     try {
