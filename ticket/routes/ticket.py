fb6aff5216c05f81a611eb2d64b87ce40410ecf0
levihoang02 <129204351+levihoang02@users.noreply.github.com>
2025-04-16 21:26:32 +0700

    a

--- ticket/routes/ticket.py
+++ ticket/routes/ticket.py
@@ -1,14 +1,20 @@
-from flask import request, jsonify, send_file, Blueprint
+from flask import jsonify, jsonify, Blueprint
 from kafka_utils.event import Event, EventType
 from database.mongo import mongo
 
 ticket_bp = Blueprint("ticket", __name__)
 
-@ticket_bp.route("/<ticket_id>", methods=["GET"])
+@ticket_bp.route("/use/<ticket_id>", methods=["GET"])
 def get_ticket_by_id(ticket_id):
     try:
-       ticket = mongo.find_one('tickets', {'id': ticket_id})
-       return ticket if ticket else "Ticket not found", 404
+        # Find ticket by 'id'
+        ticket = mongo.find_one('tickets', {'id': ticket_id})
+        
+        if ticket:
+            return jsonify(ticket), 200
+        else:
+            return jsonify({"error": "Ticket not found"}), 404
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
