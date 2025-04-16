5b331a5b2c1b094b7609b1fd5630e8226a511743
levihoang02 <129204351+levihoang02@users.noreply.github.com>
2025-04-16 21:09:07 +0700

    F gW

--- gateway/initialize.js
+++ gateway/initialize.js
@@ -2,7 +2,7 @@ const config = require('./config')();
 const serviceController = require('./controllers/service.controller');
 const { storeInstances } = require('./services/loadbalancing');
 const { initializeRedis } = require('./services/redis');
-const { SeriveInsance } = require('./models');
+const { ServiceInstance } = require('./models');
 
 const initialize = async () => {
     console.log('Inittialzing gateway...');
@@ -14,8 +14,8 @@ const initialize = async () => {
             let endPoint = service['endPoint'];
             let foundService = await serviceController.findServiceByEndPoint(endPoint);
             if (foundService) {
-                let instances = await serviceController.findInstancesByServiceId(foundService.id);
-                if (!instances) {
+                let instances = service['instances'];
+                if (instances) {
                     instances = service['instances'];
                     instances.forEach((instance) => {
                         instance['status'] = true;
@@ -24,17 +24,21 @@ const initialize = async () => {
                         instance['ServiceId'] = foundService.id;
                     });
                     for (const i of instances) {
-                        const exist = SeriveInsance.findOne({
+                        console.log('Async instances...');
+                        const exist = await ServiceInstance.findOne({
                             where: {
                                 id: i['id'],
                             },
                         });
                         if (!exist) {
-                            await SeriveInsance.create(i);
+                            await ServiceInstance.create(i);
                         }
                     }
                 }
                 instances = service['instances'];
+                instances.forEach((instance) => {
+                    instance['status'] = true;
+                });
                 await storeInstances(endPoint, instances);
             } else {
                 let instances = service['instances'];
