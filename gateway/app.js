6321415c4229e0fddc66f6f6de876e6dd47dae72
levihoang02 <129204351+levihoang02@users.noreply.github.com>
2025-04-17 19:39:32 +0700

    update cors

--- gateway/app.js
+++ gateway/app.js
@@ -6,9 +6,11 @@ const cors = require('cors');
 const routing = require('./routes/index');
 const cookieParser = require('cookie-parser');
 const { requestLogger } = require('./services/logger');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const corsOptions = {
-    origin: '*', // Allow all origins
+    origin: [process.env.UV_DESK_CLIENT, process.env.UI_CLIENT], // List all your client origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'bksi-api-key'],
     credentials: true,
